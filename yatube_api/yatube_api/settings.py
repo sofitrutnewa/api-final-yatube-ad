97e7a0f3b8a37c19691f2d426ce025b623de3af3
sofitrutnewa <sofitrutnewa406@gmail.com>
2026-05-03 23:08:06 +0300

    Update settings.py

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -94,6 +94,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
+    'UNAUTHENTICATED_USER': None,
 }
 
 SIMPLE_JWT = {
