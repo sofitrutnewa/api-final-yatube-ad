ac9fc6a7d41fa38bc594d91bcf357517cf613a67
sofitrutnewa <sofitrutnewa406@gmail.com>
2026-05-03 22:58:04 +0300

    Update serializers.py

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -23,11 +23,11 @@ class GroupSerializer(serializers.ModelSerializer):
 class CommentSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
         slug_field='username', read_only=True)
+    post = serializers.PrimaryKeyRelatedField(read_only=True)
 
     class Meta:
         model = Comment
         fields = '__all__'
-
 
 class FollowSerializer(serializers.ModelSerializer):
     user = serializers.SlugRelatedField(
